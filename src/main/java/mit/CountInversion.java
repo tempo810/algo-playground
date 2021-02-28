6c95e2b6f0754b0fcf256032e0408716bc3156a8
tempo810 <tempo810@gmail.com>
2021-02-28 19:57:14 +0800

    add CountInversion

--- src/main/java/mit/CountInversion.java
+++ src/main/java/mit/CountInversion.java
@@ -8,16 +8,16 @@ import java.util.Arrays;
 public class CountInversion {
     public static int count(int[] arr) {
         int[] input = Arrays.copyOf(arr, arr.length);
-        return countMerge(input, 0, input.length - 1);
+        return countMerged(input, 0, input.length - 1);
     }
 
-    private static int countMerge(int[] arr, int low, int high) {
+    private static int countMerged(int[] arr, int low, int high) {
         if (low >= high) {
             return 0;
         }
 
         int mid = (high + low) / 2;
-        int mergedCount = countMerge(arr, low, mid) + countMerge(arr, mid + 1, high);
+        int mergedCount = countMerged(arr, low, mid) + countMerged(arr, mid + 1, high);
         int[] left = Arrays.copyOfRange(arr, low, mid + 1);
         int[] right = Arrays.copyOfRange(arr, mid + 1, high + 1);
         int l = 0;
