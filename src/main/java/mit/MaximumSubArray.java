c1107cedc6ed2a33e8852b1db386b6febeb1cf83
tempo810 <tempo810@gmail.com>
2021-03-03 01:20:00 +0800

    add MaximumSubArray

--- src/main/java/mit/MaximumSubArray.java
+++ src/main/java/mit/MaximumSubArray.java
@@ -18,4 +18,88 @@ public class MaximumSubArray {
         }
         return days;
     }
+
+    public static int[] divideAndConquer(int[] prices) {
+        int[] delta = new int[prices.length - 1];
+        for (int i = 1; i < prices.length; i++) {
+            delta[i - 1] = prices[i] - prices[i - 1];
+        }
+        Result result = solve(0, delta.length - 1, delta);
+        return new int[]{result.low, result.high + 1};
+    }
+
+    private static Result solve(int low, int high, int[] delta) {
+        if (low == high) {
+            return new Result(low, high, delta[low]);
+        }
+
+        int mid = (low + high) / 2;
+        Result left = solve(low, mid, delta);
+        Result right = solve(mid + 1, high, delta);
+
+        int leftSum = Integer.MIN_VALUE;
+        int maxLeft = mid;
+        int sum = 0;
+        for (int i = mid; i >= low; i--) {
+            sum += delta[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                maxLeft = i;
+            }
+        }
+
+        int rightSum = Integer.MIN_VALUE;
+        int maxRight = mid + 1;
+        sum = 0;
+        for (int i = mid + 1; i <= high; i++) {
+            sum += delta[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+                maxRight = i;
+            }
+        }
+        if (left.sum >= right.sum && left.sum >= leftSum + rightSum) {
+            return left;
+        } else if (right.sum >= left.sum && right.sum >= leftSum + rightSum) {
+            return right;
+        }
+        return new Result(maxLeft, maxRight, leftSum + rightSum);
+    }
+
+    public static int[] linear(int[] prices) {
+        int[] deltas = new int[prices.length - 1];
+        for (int i = 0; i < prices.length - 1; i++) {
+            deltas[i] = prices[i + 1] - prices[i];
+        }
+
+        int currentMax = 0;
+        int currentStart = -1;
+        int globalMax = 0;
+        int globalStart = -1;
+        int globalEnd = -1;
+        for (int i = 0; i < deltas.length; i++) {
+            currentMax = Math.max(0, currentMax + deltas[i]);
+            if (currentMax == 0) {
+                currentStart = i + 1;
+            }
+            if (currentMax > globalMax) {
+                globalMax = currentMax;
+                globalStart = currentStart;
+                globalEnd = i;
+            }
+        }
+        return new int[]{globalStart, globalEnd + 1};
+    }
+
+    private static class Result {
+        private final int low;
+        private final int high;
+        private final int sum;
+
+        private Result(int low, int high, int sum) {
+            this.low = low;
+            this.high = high;
+            this.sum = sum;
+        }
+    }
 }
--- src/test/java/mit/MaximumSubArrayTest.java
+++ src/test/java/mit/MaximumSubArrayTest.java
@@ -10,9 +10,20 @@ import static org.hamcrest.core.Is.is;
  * @author tempo
  */
 class MaximumSubArrayTest {
+    private final int[] prices = new int[]{100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97};
+
     @Test
     void bruteForce() {
-        int[] prices = {100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97};
         MatcherAssert.assertThat(MaximumSubArray.bruteForce(prices), is(new int[]{7, 11}));
+    }
+
+    @Test
+    void recursion() {
+        MatcherAssert.assertThat(MaximumSubArray.divideAndConquer(prices), is(new int[]{7, 11}));
+    }
+
+    @Test
+    void linear() {
+        MatcherAssert.assertThat(MaximumSubArray.linear(prices), is(new int[]{7, 11}));
     }
 }
