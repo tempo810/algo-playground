db39442a194d72c71a4afe3d0b02381fe9163f56
tempo <tempo810@gmail.com>
2021-03-16 17:56:36 +0800

    add LeetCode BestTimeToSellOrBuyAStockWithTransactionFee

--- src/main/java/leetcode/BestTimeToSellOrBuyAStockWithTransactionFee.java
+++ src/main/java/leetcode/BestTimeToSellOrBuyAStockWithTransactionFee.java
@@ -5,17 +5,13 @@ package leetcode;
  */
 public class BestTimeToSellOrBuyAStockWithTransactionFee {
     public int maxProfit(int[] prices, int fee) {
-        int prevSell = 0;
-        int prevBuy = 0;
-        int endSell = 0;
+        int profit = 0;
         int endBuy = Integer.MIN_VALUE;
 
         for (int price : prices) {
-            prevBuy = endBuy;
-            endBuy = Math.max(endBuy, prevSell - price - fee);
-            prevSell = endSell;
-            endSell = Math.max(endSell, prevBuy + price);
+            endBuy = Math.max(endBuy, profit - price - fee); // compete on minimum buying price
+            profit = Math.max(profit, endBuy + price);
         }
-        return endSell;
+        return profit;
     }
 }
