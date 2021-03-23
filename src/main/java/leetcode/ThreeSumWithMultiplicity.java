0cc73b6459689cf9262926a2626fa31d1c8ac5d6
tempo <tempo810@gmail.com>
2021-03-23 18:52:13 +0800

    add ThreeSumWithMultiplicity

--- src/main/java/leetcode/ThreeSumWithMultiplicity.java
+++ src/main/java/leetcode/ThreeSumWithMultiplicity.java
@@ -1,23 +1,59 @@
 package leetcode;
 
-import java.math.BigInteger;
-
 /**
  * @author tempo
  */
 public class ThreeSumWithMultiplicity {
-    static BigInteger nCr(int n, int r) {
-        return fact(n).divide(fact(r)).multiply(
-                fact(n - r));
+    static long printNcR(int n, int r) {
+
+        // p holds the value of n*(n-1)*(n-2)...,
+        // k holds the value of r*(r-1)...
+        long p = 1, k = 1;
+
+        // C(n, r) == C(n, n-r),
+        // choosing the smaller value
+        if (n - r < r) {
+            r = n - r;
+        }
+
+        if (r != 0) {
+            while (r > 0) {
+                p *= n;
+                k *= r;
+
+                // gcd of p, k
+                long m = __gcd(p, k);
+
+                // dividing by gcd, to simplify
+                // product division by their gcd
+                // saves from the overflow
+                p /= m;
+                k /= m;
+
+                n--;
+                r--;
+            }
+
+            // k should be simplified to 1
+            // as C(n, r) is a natural number
+            // (denominator should be 1 ) .
+        } else {
+            p = 1;
+        }
+
+        return p;
     }
 
-    // Returns factorial of n
-    static BigInteger fact(int n) {
-        BigInteger res = BigInteger.ONE;
-        for (int i = 2; i <= n; i++) {
-            res = res.multiply(BigInteger.valueOf(i));
+    static long __gcd(long n1, long n2) {
+        long gcd = 1;
+
+        for (int i = 1; i <= n1 && i <= n2; ++i) {
+            // Checks if i is factor of both integers
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
         }
-        return res;
+        return gcd;
     }
 
     public int threeSumMultiV2(int[] arr, int target) {
@@ -35,19 +71,18 @@ public class ThreeSumWithMultiplicity {
         int possibleDigit = 3 - digit;
         int count = 0;
         if (possibleDigit * index == target && possibleDigit <= occurrence[index]) {
-            BigInteger countV1 = nCr(occurrence[index], possibleDigit);
-            count = rounding(countV1);
+            count = rounding(printNcR(occurrence[index], possibleDigit));
         }
         possibleDigit = Math.min(occurrence[index], possibleDigit);
 
         while (possibleDigit >= 0) {
-            count = rounding(BigInteger.valueOf(count).add(nCr(occurrence[index], possibleDigit).multiply(BigInteger.valueOf(helperV2(occurrence, index + 1, target - index * possibleDigit, digit + possibleDigit)))));
+            count = rounding(count + printNcR(occurrence[index], possibleDigit) * helperV2(occurrence, index + 1, target - index * possibleDigit, digit + possibleDigit));
             possibleDigit--;
         }
         return count;
     }
 
-    private int rounding(BigInteger count) {
-        return count.mod(BigInteger.valueOf((long) Math.pow(10, 9) + 7)).intValue();
+    private int rounding(long count) {
+        return (int) (count % 1_000_000_007);
     }
 }
