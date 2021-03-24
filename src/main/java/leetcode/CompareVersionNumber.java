f74ff814b035bb3062637f95fe64829030584766
tempo810 <tempo810@gmail.com>
2021-03-25 03:08:40 +0800

    add CompareVersionNumber

--- src/main/java/leetcode/CompareVersionNumber.java
+++ src/main/java/leetcode/CompareVersionNumber.java
@@ -6,9 +6,7 @@ package leetcode;
 public class CompareVersionNumber {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
-        int v1Sum = 0;
         String[] v2 = version2.split("\\.");
-        int v2Sum = 0;
 
         for (int i = 0; i < Math.min(v1.length, v2.length); i++) {
             int rev1 = Integer.parseInt(v1[i]);
@@ -16,21 +14,23 @@ public class CompareVersionNumber {
             if (rev1 != rev2) {
                 return Integer.compare(rev1, rev2);
             }
-            v1Sum += rev1;
-            v2Sum += rev2;
         }
 
         if (v1.length > v2.length) {
             for (int i = v2.length; i < v1.length; i++) {
-                v1Sum += Integer.parseInt(v1[i]);
+                if (Integer.parseInt(v1[i]) > 0) {
+                    return 1;
+                }
             }
         }
 
         if (v2.length > v1.length) {
             for (int i = v1.length; i < v2.length; i++) {
-                v2Sum += Integer.parseInt(v2[i]);
+                if (Integer.parseInt(v2[i]) > 0) {
+                    return -1;
+                }
             }
         }
-        return Integer.compare(v1Sum, v2Sum);
+        return 0;
     }
 }
