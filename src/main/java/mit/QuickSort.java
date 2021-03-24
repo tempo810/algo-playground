469ab2e6897b65594cf18ce6ad55905f89662f4c
tempo810 <tempo810@gmail.com>
2021-03-24 23:45:27 +0800

    update QuickSort

--- src/main/java/mit/QuickSort.java
+++ src/main/java/mit/QuickSort.java
@@ -4,6 +4,34 @@ package mit;
  * @author tempo
  */
 public class QuickSort {
+    public static void sortHoare(int[] arr, int start, int end) {
+        if (end > start) {
+            int pivot = partitionHoare(arr, start, end);
+            sortHoare(arr, start, pivot - 1);
+            sortHoare(arr, pivot + 1, end);
+        }
+    }
+
+    private static int partitionHoare(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int low = start;
+        int high = end;
+        while (low < high) {
+            while (arr[high] > pivot) {
+                high--;
+            }
+            while (arr[low] < pivot) {
+                low++;
+            }
+            if (low < high) {
+                int temp = arr[low];
+                arr[low] = arr[high];
+                arr[high] = temp;
+            }
+        }
+        return high;
+    }
+
     public static void sort(int[] arr, int start, int end) {
         if (end > start) {
             int pivot = partition(arr, start, end);
--- src/test/java/mit/QuickSortTest.java
+++ src/test/java/mit/QuickSortTest.java
@@ -15,5 +15,9 @@ class QuickSortTest {
         int[] arr = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
         QuickSort.sort(arr, 0, arr.length - 1);
         assertThat(arr, Matchers.is(new int[]{1, 2, 3, 4, 7, 8, 9, 10, 14, 16}));
+
+        int[] arr2 = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        QuickSort.sortHoare(arr2, 0, arr2.length - 1);
+        assertThat(arr2, Matchers.is(new int[]{1, 2, 3, 4, 7, 8, 9, 10, 14, 16}));
     }
 }
