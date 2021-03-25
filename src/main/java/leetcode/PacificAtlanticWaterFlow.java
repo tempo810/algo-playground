3ebafe653864ac6f76541b09e1efd9fef9330d44
tempo810 <tempo810@gmail.com>
2021-03-26 02:04:31 +0800

    add PacificAtlanticWaterFlow

--- src/main/java/leetcode/PacificAtlanticWaterFlow.java
+++ src/main/java/leetcode/PacificAtlanticWaterFlow.java
@@ -7,6 +7,28 @@ import java.util.List;
  * @author tempo
  */
 public class PacificAtlanticWaterFlow {
+    public List<List<Integer>> pacificAtlanticV2(int[][] matrix) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (matrix.length == 0) {
+            return results;
+        }
+
+        boolean[][] pacific = new boolean[matrix.length][matrix[0].length];
+        boolean[][] atlanta = new boolean[matrix.length][matrix[0].length];
+
+        dfsV2(0, 0, Integer.MIN_VALUE, matrix, pacific, 0, 0);
+        dfsV2(matrix.length - 1, matrix[0].length - 1, Integer.MIN_VALUE, matrix, atlanta, matrix.length - 1, matrix[0].length - 1);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (pacific[i][j] && atlanta[i][j]) {
+                    results.add(List.of(i, j));
+                }
+            }
+        }
+        return results;
+    }
+
     public List<List<Integer>> pacificAtlantic(int[][] matrix) {
         List<List<Integer>> results = new ArrayList<>();
         if (matrix.length == 0) {
@@ -40,12 +62,27 @@ public class PacificAtlanticWaterFlow {
         if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[i].length || ocean[i][j] == 1) {
             return;
         }
+
         if (matrix[i][j] >= lastValue) {
             ocean[i][j] = 1;
+            dfs(i + 1, j, matrix[i][j], matrix, ocean);
+            dfs(i - 1, j, matrix[i][j], matrix, ocean);
+            dfs(i, j + 1, matrix[i][j], matrix, ocean);
+            dfs(i, j - 1, matrix[i][j], matrix, ocean);
         }
-        dfs(i + 1, j, matrix[i][j], matrix, ocean);
-        dfs(i - 1, j, matrix[i][j], matrix, ocean);
-        dfs(i, j + 1, matrix[i][j], matrix, ocean);
-        dfs(i, j - 1, matrix[i][j], matrix, ocean);
+    }
+
+    private void dfsV2(int i, int j, int lastValue, int[][] matrix, boolean[][] ocean, int edgeI, int edgeJ) {
+        if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[i].length || ocean[i][j]) {
+            return;
+        }
+
+        if (i == edgeI || j == edgeJ || matrix[i][j] >= lastValue) {
+            ocean[i][j] = true;
+            dfsV2(i + 1, j, matrix[i][j], matrix, ocean, edgeI, edgeJ);
+            dfsV2(i - 1, j, matrix[i][j], matrix, ocean, edgeI, edgeJ);
+            dfsV2(i, j + 1, matrix[i][j], matrix, ocean, edgeI, edgeJ);
+            dfsV2(i, j - 1, matrix[i][j], matrix, ocean, edgeI, edgeJ);
+        }
     }
 }
