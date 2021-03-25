83604a8ca989f42cfa22054d8fc7543a6a25cc5b
tempo810 <tempo810@gmail.com>
2021-03-25 23:03:51 +0800

    fix PacificAtlanticWaterFlow

--- src/main/java/leetcode/PacificAtlanticWaterFlow.java
+++ src/main/java/leetcode/PacificAtlanticWaterFlow.java
@@ -13,18 +13,22 @@ public class PacificAtlanticWaterFlow {
             return results;
         }
 
-        Boolean[][] pacificDp = new Boolean[matrix.length][matrix[0].length];
-        Boolean[][] atlanticDp = new Boolean[matrix.length][matrix[0].length];
+        int[][] pacific = new int[matrix.length][matrix[0].length];
+        int[][] atlanta = new int[matrix.length][matrix[0].length];
+
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                topLeft(pacificDp, matrix, i, j);
-                bottomRight(atlanticDp, matrix, i, j);
-            }
+            dfs(i, 0, Integer.MIN_VALUE, matrix, pacific);
+            dfs(i, matrix[i].length - 1, Integer.MIN_VALUE, matrix, atlanta);
+        }
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            dfs(0, i, Integer.MIN_VALUE, matrix, pacific);
+            dfs(matrix.length - 1, i, Integer.MIN_VALUE, matrix, atlanta);
         }
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (canReach(i, j, pacificDp, atlanticDp) && canReach(i, j, atlanticDp, pacificDp)) {
+                if (pacific[i][j] == 1 && atlanta[i][j] == 1) {
                     results.add(List.of(i, j));
                 }
             }
@@ -32,84 +36,16 @@ public class PacificAtlanticWaterFlow {
         return results;
     }
 
-    private boolean canReach(int i, int j, Boolean[][] targetPath, Boolean[][] backupPath) {
-        if (!rangeCheck(i, j, targetPath)) {
-            return false;
+    private void dfs(int i, int j, int lastValue, int[][] matrix, int[][] ocean) {
+        if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[i].length || ocean[i][j] == 1) {
+            return;
         }
-
-        if (targetPath[i][j]) {
-            return true;
+        if (matrix[i][j] >= lastValue) {
+            ocean[i][j] = 1;
         }
-
-        if (rangeCheck(i - 1, j, targetPath) && backupPath[i - 1][j]) {
-            backupPath[i - 1][j] = false;
-            boolean result = canReach(i - 1, j, targetPath, backupPath);
-            backupPath[i - 1][j] = true;
-            if (result) {
-                return true;
-            }
-        }
-        if (rangeCheck(i + 1, j, targetPath) && backupPath[i + 1][j]) {
-            backupPath[i + 1][j] = false;
-            boolean result = canReach(i + 1, j, targetPath, backupPath);
-            backupPath[i + 1][j] = true;
-            if (result) {
-                return true;
-            }
-        }
-        if (rangeCheck(i, j + 1, targetPath) && backupPath[i][j + 1]) {
-            backupPath[i][j + 1] = false;
-            boolean result = canReach(i, j + 1, targetPath, backupPath);
-            backupPath[i][j + 1] = true;
-            if (result) {
-                return true;
-            }
-        }
-
-        if (rangeCheck(i, j - 1, targetPath) && backupPath[i][j - 1]) {
-            backupPath[i][j - 1] = false;
-            boolean result = canReach(i, j - 1, targetPath, backupPath);
-            backupPath[i][j - 1] = true;
-            return result;
-        }
-
-        return false;
-    }
-
-    private boolean rangeCheck(int i, int j, Boolean[][] backupPath) {
-        return i >= 0 && j >= 0 && i < backupPath.length && j < backupPath[i].length;
-    }
-
-    private boolean topLeft(Boolean[][] dp, int[][] matrix, int i, int j) {
-        if (dp[i][j] == null) {
-            if (i == 0 || j == 0) {
-                dp[i][j] = true;
-            } else {
-                if (i > 0) {
-                    dp[i][j] = matrix[i][j] >= matrix[i - 1][j] && topLeft(dp, matrix, i - 1, j);
-                }
-                if (j > 0 && !dp[i][j]) {
-                    dp[i][j] = matrix[i][j] >= matrix[i][j - 1] && topLeft(dp, matrix, i, j - 1);
-                }
-            }
-        }
-        return dp[i][j];
-    }
-
-
-    private boolean bottomRight(Boolean[][] dp, int[][] matrix, int i, int j) {
-        if (dp[i][j] == null) {
-            if (i == matrix.length - 1 || j == matrix[i].length - 1) {
-                dp[i][j] = true;
-            } else {
-                if (i < matrix.length - 1) {
-                    dp[i][j] = matrix[i][j] >= matrix[i + 1][j] && bottomRight(dp, matrix, i + 1, j);
-                }
-                if (j < matrix[i].length - 1 && !dp[i][j]) {
-                    dp[i][j] = matrix[i][j] >= matrix[i][j + 1] && bottomRight(dp, matrix, i, j + 1);
-                }
-            }
-        }
-        return dp[i][j];
+        dfs(i + 1, j, matrix[i][j], matrix, ocean);
+        dfs(i - 1, j, matrix[i][j], matrix, ocean);
+        dfs(i, j + 1, matrix[i][j], matrix, ocean);
+        dfs(i, j - 1, matrix[i][j], matrix, ocean);
     }
 }
