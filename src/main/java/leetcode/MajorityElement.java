4ea0d952debb740a4d9e2ed675224c86217afdd7
tempo <tempo810@gmail.com>
2021-03-26 10:36:14 +0800

    add MajorityElement

--- src/main/java/leetcode/MajorityElement.java
+++ src/main/java/leetcode/MajorityElement.java
@@ -8,19 +8,6 @@ import java.util.Arrays;
 public class MajorityElement {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        int count = 1;
-        int currentNum = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == currentNum) {
-                count++;
-            } else {
-                currentNum = nums[i];
-                count = 1;
-            }
-            if (count >= Math.ceil((float) nums.length / 2)) {
-                break;
-            }
-        }
-        return currentNum;
+        return nums[nums.length / 2];
     }
 }
