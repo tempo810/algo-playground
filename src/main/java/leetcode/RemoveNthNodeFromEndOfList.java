ba61413eb0e6f9746ff78be89b4ba01fd9adc134
tempo810 <tempo810@gmail.com>
2021-04-18 19:25:01 +0800

    add RemoveNthNodeFromEndOfList

---
+++ src/main/java/leetcode/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import leetcode.datastructure.ListNode;
+
+/**
+ * @author Tempo
+ */
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i < n && fast != null; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
