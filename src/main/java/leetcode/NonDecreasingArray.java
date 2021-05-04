cf64a61b6d178b1db42d59fb99506700ec42ceab
tempo810 <tempo810@gmail.com>
2021-05-05 00:44:40 +0800

    fix NonDecreasingArray

--- src/main/java/leetcode/NonDecreasingArray.java
+++ src/main/java/leetcode/NonDecreasingArray.java
@@ -9,7 +9,7 @@ public class NonDecreasingArray {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < nums[i - 1]) {
                 if (modified) {
-                    return true;
+                    return false;
                 } else {
                     boolean isPeak = i + 1 >= nums.length || nums[i - 1] <= nums[i + 1];
                     boolean isBottom = i - 2 < 0 || nums[i - 2] <= nums[i];
