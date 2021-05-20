faf8cb7b52a96a8715f62dd1f72acb7f209faeab
tempo810 <tempo810@gmail.com>
2021-05-20 20:45:44 +0800

    optimise MinimumMoveToEqualElementArray

--- src/main/java/leetcode/MinimumMoveToEqualElementArray.java
+++ src/main/java/leetcode/MinimumMoveToEqualElementArray.java
@@ -8,18 +8,10 @@ import java.util.Arrays;
 public class MinimumMoveToEqualElementArray {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
-        int[] dp = new int[nums.length];
+        int median = nums[nums.length / 2];
         int totalMove = 0;
-        for (int i = 1; i <= nums.length / 2; i++) {
-            int diff = nums[nums.length / 2 - (i - 1)] - nums[(nums.length / 2) - i];
-            dp[(nums.length / 2) - i] = diff + dp[nums.length / 2 - (i - 1)];
-            totalMove += dp[(nums.length / 2) - i];
-
-            if ((nums.length / 2) + i < nums.length) {
-                diff = nums[(nums.length / 2) + i] - nums[nums.length / 2 + (i - 1)];
-                dp[(nums.length / 2) + i] = diff + dp[nums.length / 2 + (i - 1)];
-                totalMove += dp[(nums.length / 2) + i];
-            }
+        for (int i = 0; i < nums.length; i++) {
+            totalMove += Math.abs(nums[i] - median);
         }
         return totalMove;
     }
