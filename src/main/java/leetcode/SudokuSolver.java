91b732de4209d2808ed7280d5b95c219c14bab58
tempo810 <tempo810@gmail.com>
2021-05-23 06:11:02 +0800

    optimise SudokuSolver

--- src/main/java/leetcode/SudokuSolver.java
+++ src/main/java/leetcode/SudokuSolver.java
@@ -1,14 +1,11 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 /**
  * @author Tempo
  */
 public class SudokuSolver {
-    private final Set<Character> CANDIDATES = Set.of('1', '2', '3', '4', '5', '6', '7', '8', '9');
-
     public void solveSudoku(char[][] board) {
         solve(board, 0, 0);
     }
@@ -21,26 +18,35 @@ public class SudokuSolver {
         if (board[row][col] != '.') {
             return col == 8 ? solve(board, row + 1, 0) : solve(board, row, col + 1);
         }
-
-        Set<Character> candidates = new HashSet<>(CANDIDATES);
+        boolean[] candidates = new boolean[9];
+        Arrays.fill(candidates, true);
         for (int i = 0; i < 9; i++) {
-            candidates.remove(board[row][i]);
-            candidates.remove(board[i][col]);
+            if (board[row][i] != '.') {
+                candidates[board[row][i] - 49] = false;
+            }
+            if (board[i][col] != '.') {
+                candidates[board[i][col] - 49] = false;
+            }
         }
         int squareX = col / 3 * 3;
         int squareY = row / 3 * 3;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                candidates.remove(board[i + squareY][j + squareX]);
+                if (board[i + squareY][j + squareX] != '.') {
+                    candidates[board[i + squareY][j + squareX] - 49] = false;
+                }
             }
         }
 
-        for (Character candidate : candidates) {
-            board[row][col] = candidate;
-            if (col == 8 ? solve(board, row + 1, 0) : solve(board, row, col + 1)) {
-                return true;
+        for (int i = 0; i < candidates.length; i++) {
+            if (candidates[i]) {
+                board[row][col] = (char) (i + 49);
+                if (col == 8 ? solve(board, row + 1, 0) : solve(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = '.';
+
             }
-            board[row][col] = '.';
         }
         return false;
     }
