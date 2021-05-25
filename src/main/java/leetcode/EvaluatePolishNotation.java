e1228c5379e9163b1b5602325f744f10eb52335c
tempo810 <tempo810@gmail.com>
2021-05-25 21:19:05 +0800

    add EvaluatePolishNotation

--- src/main/java/leetcode/EvaluatePolishNotation.java
+++ src/main/java/leetcode/EvaluatePolishNotation.java
@@ -6,11 +6,6 @@ import java.util.Stack;
  * @author Tempo
  */
 public class EvaluatePolishNotation {
-    public static void main(String[] args) {
-        var solution = new EvaluatePolishNotation();
-        System.out.println(solution.evalRPN(new String[]{"4", "13", "5", "/", "+"}));
-    }
-
     public int evalRPN(String[] tokens) {
         Stack<Integer> digits = new Stack<>();
         for (String token : tokens) {
