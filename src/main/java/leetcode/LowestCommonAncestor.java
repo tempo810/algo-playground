dfed3420a901134fdc765a40bc869fbc782c3611
tempo810 <tempo810@gmail.com>
2021-07-20 01:20:06 +0800

    add LowestCommonAncestor

---
+++ src/main/java/leetcode/LowestCommonAncestor.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import leetcode.datastructure.TreeNode;
+
+/**
+ * @author Tempo
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
