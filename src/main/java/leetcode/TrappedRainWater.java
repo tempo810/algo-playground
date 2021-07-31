2e43e13c92e5ff321a5e028ab5e6e7e9bff060fa
tempo810 <tempo810@gmail.com>
2021-07-31 17:03:01 +0800

    add leetcode answer

--- src/main/java/leetcode/TrappedRainWater.java
+++ src/main/java/leetcode/TrappedRainWater.java
@@ -24,4 +24,26 @@ public class TrappedRainWater {
         }
         return trappedVolume;
     }
+
+    public int trapV2(int[] height) {
+        if (height.length < 3) {
+            return 0;
+        }
+        int[] leftMaxPrefix = new int[height.length];
+        leftMaxPrefix[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMaxPrefix[i] = Math.max(leftMaxPrefix[i - 1], height[i]);
+        }
+
+        int[] rightMaxPrefix = new int[height.length];
+        rightMaxPrefix[rightMaxPrefix.length - 1] = height[height.length - 1];
+        for (int i = rightMaxPrefix.length - 2; i >= 0; i--) {
+            rightMaxPrefix[i] = Math.max(rightMaxPrefix[i + 1], height[i]);
+        }
+        int result = 0;
+        for (int i = 0; i < height.length; i++) {
+            result += Math.min(leftMaxPrefix[i], rightMaxPrefix[i]) - height[i];
+        }
+        return result;
+    }
 }
