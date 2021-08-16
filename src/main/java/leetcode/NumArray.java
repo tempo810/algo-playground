27b1d505c057ee8fb1334497786b0a1a4d3bac68
tempo810 <tempo810@gmail.com>
2021-08-16 19:56:05 +0800

    add NumArray

---
+++ src/main/java/leetcode/NumArray.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * @author Tempo
+ */
+public class NumArray {
+    private final int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
