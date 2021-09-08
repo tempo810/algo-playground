3cd71765ad5d25253f745fe6fcdfa3c208ab68b0
tempo <tempo810@gmail.com>
2021-09-08 17:49:50 +0800

    add ShiftingLetters

---
+++ src/main/java/leetcode/datastructure/ShiftingLetters.java
@@ -0,0 +1,20 @@
+package leetcode.datastructure;
+
+/**
+ * @author Tempo
+ */
+public class ShiftingLetters {
+    public String shiftingLetters(String s, int[] shifts) {
+        char[] chars = s.toCharArray();
+        int shift = 0;
+        for (int i = shifts.length - 1; i >= 0; i--) {
+            shift = (shift + shifts[i]) % 26;
+            int shiftedChar = (chars[i] + shift) % 123;
+            if (shiftedChar < 97) {
+                shiftedChar += 97;
+            }
+            chars[i] = (char) shiftedChar;
+        }
+        return new String(chars);
+    }
+}
