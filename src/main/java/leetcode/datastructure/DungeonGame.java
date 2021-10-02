577ed509eb043808ca48aca0f86abc73f8d18263
tempo810 <tempo810@gmail.com>
2021-10-02 18:56:38 +0800

    add DungeonGame

---
+++ src/main/java/leetcode/datastructure/DungeonGame.java
@@ -0,0 +1,24 @@
+package leetcode.datastructure;
+
+/**
+ * @author Tempo
+ */
+public class DungeonGame {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int[][] dp = new int[dungeon.length][dungeon[0].length];
+        for (int i = dungeon.length - 1; i >= 0; i--) {
+            for (int j = dungeon[0].length - 1; j >= 0; j--) {
+                if (i == dungeon.length - 1 && j == dungeon[0].length - 1) {
+                    dp[i][j] = Math.min(0, dungeon[i][j]);
+                } else if (i == dungeon.length - 1) {
+                    dp[i][j] = Math.min(0, dungeon[i][j] + dp[i][j + 1]);
+                } else if (j == dungeon[0].length - 1) {
+                    dp[i][j] = Math.min(0, dungeon[i][j] + dp[i + 1][j]);
+                } else {
+                    dp[i][j] = Math.min(0, dungeon[i][j] + Math.max(dp[i + 1][j], dp[i][j + 1]));
+                }
+            }
+        }
+        return Math.abs(dp[0][0]) + 1;
+    }
+}
---
+++ src/test/java/leetcode/datastructure/DungeonGameTest.java
@@ -0,0 +1,17 @@
+package leetcode.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Tempo
+ */
+class DungeonGameTest {
+    private final DungeonGame solution = new DungeonGame();
+
+    @Test
+    void calculateMinimumHP() {
+        assertThat(solution.calculateMinimumHP(new int[][]{{-2, -3, 3}, {-5, -10, 1}, {10, 30, -5}})).isEqualTo(7);
+    }
+}
