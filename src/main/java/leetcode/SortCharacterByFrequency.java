042040feec873370e5f3a0a72c51e6d1e355cc3e
tempo810 <tempo810@gmail.com>
2021-10-23 00:43:46 +0800

    add SortCharacterByFrequency

---
+++ src/main/java/leetcode/SortCharacterByFrequency.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author Tempo
+ */
+public class SortCharacterByFrequency {
+    public String frequencySort(String s) {
+        char[] chars = s.toCharArray();
+        int[] frequency = new int[80];
+        for (char c : chars) {
+            frequency[c - '0']++;
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] != 0) {
+                heap.add(frequency[i] * 100 + i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!heap.isEmpty()) {
+            Integer value = heap.poll();
+            char c = (char) (value % 100 + '0');
+            for (int i = 0; i < value / 100; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
