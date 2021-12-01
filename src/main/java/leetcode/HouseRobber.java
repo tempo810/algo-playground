1776b77464442c62af98d4c6553254078af5b3d4
tempo810 <tempo810@gmail.com>
2021-12-02 00:29:57 +0800

    revisit HouseRobber

--- src/main/java/leetcode/HouseRobber.java
+++ src/main/java/leetcode/HouseRobber.java
@@ -5,18 +5,32 @@ package leetcode;
  */
 public class HouseRobber {
     public int rob(int[] nums) {
-        if (nums.length <= 1) {
-            return nums.length == 0 ? 0 : nums[0];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+
+    public int robV2(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-        int[] dp = new int[nums.length];
-        dp[dp.length - 1] = nums[nums.length - 1];
-        dp[dp.length - 2] = nums[nums.length - 2];
-        int maxStolen = Math.max(dp[dp.length - 1], dp[dp.length - 2]);
-        for (int i = nums.length - 3; i >= 0; i--) {
-            dp[i] = Math.max(dp[i + 2], i + 3 < nums.length ? dp[i + 3] : 0) + nums[i];
-            maxStolen = Math.max(maxStolen, dp[i]);
+        int currentBest = Math.max(nums[0], nums[1]);
+        int lastBest = nums[0];
+
+        for (int i = 2; i < nums.length; i++) {
+            int temp = Math.max(lastBest + nums[i], currentBest);
+            lastBest = currentBest;
+            currentBest = temp;
         }
-        return maxStolen;
+
+        return currentBest;
     }
 }
