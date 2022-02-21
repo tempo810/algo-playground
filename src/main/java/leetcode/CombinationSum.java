78906e56d6bfb8d219f2c9770fa167eb2fdad405
tempo810 <tempo810@gmail.com>
2022-02-22 00:58:00 +0800

    update MajorityElement

--- src/main/java/leetcode/CombinationSum.java
+++ src/main/java/leetcode/CombinationSum.java
@@ -5,11 +5,52 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author Tempo
  */
 public class CombinationSum {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        Stack<Integer> partial = new Stack<>();
+        findSum(candidates, 0, target, partial, result);
+        return result;
+    }
+
+    private void findSum(int[] candidates, int index, int remainingSum, Stack<Integer> partial, List<List<Integer>> result) {
+        if (index == candidates.length) {
+            return;
+        }
+
+        int candidate = candidates[index];
+        if (candidate > remainingSum) {
+            return;
+        }
+
+        int dividend = remainingSum / candidate;
+        if (remainingSum % candidate == 0) {
+            List<Integer> currentResult = new ArrayList<>(partial);
+            for (int i = 0; i < dividend; i++) {
+                currentResult.add(candidate);
+            }
+            result.add(currentResult);
+        }
+
+        int current = 1;
+        for (; current <= dividend && candidate * current < remainingSum; current++) {
+            partial.add(candidate);
+            findSum(candidates, index + 1, remainingSum - candidate * current, partial, result);
+        }
+
+        for (int i = 1; i < current; i++) {
+            partial.pop();
+        }
+
+        findSum(candidates, index + 1, remainingSum, partial, result);
+    }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         int[] ints = Arrays.stream(candidates).sorted().distinct().toArray();
         Deque<Integer> partial = new ArrayDeque<>();
--- src/main/java/leetcode/MajorityElement.java
+++ src/main/java/leetcode/MajorityElement.java
@@ -1,13 +1,22 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * @author tempo
  */
 public class MajorityElement {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int majority = -1;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                count++;
+                majority = num;
+            } else if (num == majority) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return majority;
     }
 }
