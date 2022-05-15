0e6e1dd346f447643c223f2049b41dbf7cf54dad
tempo810 <tempo810@gmail.com>
2022-05-15 18:34:58 +0800

    update DeepestLeaveSum

--- src/main/java/leetcode/DeepestLeaveSum.java
+++ src/main/java/leetcode/DeepestLeaveSum.java
@@ -15,6 +15,33 @@ public class DeepestLeaveSum {
         return ints.isEmpty() ? 0 : ints.get(ints.size() - 1);
     }
 
+    public int deepestLeavesSum2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int height = findHeight(root);
+        return sum(root, 1, height);
+    }
+
+    private int sum(TreeNode root, int depth, int targetDepth) {
+        if (root == null) {
+            return 0;
+        }
+        if (depth == targetDepth) {
+            return root.val;
+        } else {
+            return sum(root.left, depth + 1, targetDepth) + sum(root.right, depth + 1, targetDepth);
+        }
+    }
+
+    private int findHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(findHeight(root.left), findHeight(root.right));
+    }
+
+
     private void dfs(int depth, TreeNode root, List<Integer> ints) {
         if (root != null) {
             if (ints.size() == depth) {
