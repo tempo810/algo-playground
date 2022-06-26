fab5ae4a7c230658e123d42fe3e7732d25c901ca
tempo810 <tempo810@gmail.com>
2022-06-26 21:00:54 +0800

    add prefix sum approach for MaximumPointsYouCanObtainFromCards

--- src/main/java/leetcode/MaximumPointsYouCanObtainFromCards.java
+++ src/main/java/leetcode/MaximumPointsYouCanObtainFromCards.java
@@ -21,4 +21,26 @@ public class MaximumPointsYouCanObtainFromCards {
         }
         return total - minRemovalWindow;
     }
+
+    public int maxScorePrefixSum(int[] cardPoints, int k) {
+        if (k == 0) {
+            return 0;
+        }
+
+        int[] prefix = new int[k + 1];
+        for (int i = 0; i < k; i++) {
+            prefix[i + 1] = prefix[i] + cardPoints[i];
+        }
+
+        int[] suffix = new int[k + 1];
+        for (int i = 0; i < k; i++) {
+            suffix[i + 1] = suffix[i] + cardPoints[cardPoints.length - 1 - i];
+        }
+
+        int maxScore = 0;
+        for (int i = 0; i <= k; i++) {
+            maxScore = Math.max(maxScore, prefix[i] + suffix[k - i]);
+        }
+        return maxScore;
+    }
 }
