282b0329d5b9d8976384fde7166d6bbc9df2715e
tempo810 <tempo810@gmail.com>
2022-07-08 03:18:17 +0800

    update InterleavingString to 2D dp solution

--- src/main/java/leetcode/InterleavingString.java
+++ src/main/java/leetcode/InterleavingString.java
@@ -5,19 +5,26 @@ package leetcode;
  */
 public class InterleavingString {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
+        if (s1.length() + s2.length() == s3.length()) {
+            Boolean[][] dp = new Boolean[s1.length() + 1][s2.length() + 1];
+            return dfs(dp, s1.toCharArray(), 0, s2.toCharArray(), 0, s3.toCharArray());
         }
-        return dfs(s1, 0, s2, 0, s3, 0);
+        return false;
     }
 
-    private boolean dfs(String s1, int s1Index, String s2, int s2Index, String s3, int s3Index) {
-        if (s3Index == s3.length()) {
+    private boolean dfs(Boolean[][] dp, char[] a, int aIndex, char[] b, int bIndex, char[] target) {
+        if (aIndex + bIndex == target.length) {
             return true;
         }
-        if (s1Index < s1.length() && s1.charAt(s1Index) == s3.charAt(s3Index) && dfs(s1, s1Index + 1, s2, s2Index, s3, s3Index + 1)) {
-            return true;
+        if (dp[aIndex][bIndex] == null) {
+            dp[aIndex][bIndex] = false;
+            if (aIndex < a.length && a[aIndex] == target[aIndex + bIndex]) {
+                dp[aIndex][bIndex] = dfs(dp, a, aIndex + 1, b, bIndex, target);
+            }
+            if (!dp[aIndex][bIndex] && bIndex < b.length && b[bIndex] == target[aIndex + bIndex]) {
+                dp[aIndex][bIndex] = dfs(dp, a, aIndex, b, bIndex + 1, target);
+            }
         }
-        return s2Index < s2.length() && s2.charAt(s2Index) == s3.charAt(s3Index) && dfs(s1, s1Index, s2, s2Index + 1, s3, s3Index + 1);
+        return dp[aIndex][bIndex];
     }
 }
