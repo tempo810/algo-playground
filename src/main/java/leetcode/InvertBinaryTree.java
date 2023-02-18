7bc7f8780626662522275c76b6d055143289e8cc
tempo810 <tempo810@gmail.com>
2023-02-18 01:41:36 -0800

    add InvertBinaryTree

---
+++ src/main/java/leetcode/InvertBinaryTree.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import leetcode.datastructure.TreeNode;
+
+/**
+ * @author tempo
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root != null) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        return root;
+    }
+}
