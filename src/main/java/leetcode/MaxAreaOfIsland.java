e2a6ff2e926a95173f1736ee78c153e5b9d0a177
tempo810 <tempo810@gmail.com>
2023-02-19 01:27:32 -0800

    update MaxAreaOfIsland

--- src/main/java/leetcode/MaxAreaOfIsland.java
+++ src/main/java/leetcode/MaxAreaOfIsland.java
@@ -4,6 +4,32 @@ package leetcode;
  * @author Tempo
  */
 public class MaxAreaOfIsland {
+    private static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public int maxAreaOfIsland2(int[][] grid) {
+        int maxArea = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                maxArea = Math.max(maxArea, search(i, j, grid));
+            }
+        }
+        return maxArea;
+    }
+
+    private int search(int n, int m, int[][] grid) {
+        if (n < 0 || m < 0 || n == grid.length || m == grid[0].length || grid[n][m] == 0) {
+            return 0;
+        }
+        grid[n][m] = 0;
+        int size = 0;
+        for (int[] direction : DIRECTIONS) {
+            size += search(n + direction[0], m + direction[1], grid);
+        }
+        return size;
+    }
+
     public int maxAreaOfIsland(int[][] grid) {
         boolean[][] seen = new boolean[grid.length][grid[0].length];
         int max = 0;
